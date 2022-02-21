5d54112c774d9da373fea4171a28f5f43c0d9a8a
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-02-21 16:37:06 +0100

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,3 +1,3 @@
-yay -Sy --noconfirm sddm plasma-meta packagekit-qt5 ufw firefox vlc alacritty lutris steam wine-stable discord gparted mc nano && \
+yay -Sy --noconfirm sddm plasma-meta ufw firefox vlc alacritty lutris steam wine-stable discord gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable ufw
