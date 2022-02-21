e5a02c3908f60f88c56a5f1cafe802a45bd11c76
CambonOS <82413153+CambonOS@users.noreply.github.com>
2022-02-21 13:26:49 +0100

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,5 +1,5 @@
-yay -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins sweet-kde-git dolphin-root packagekit-qt5 ufw firefox vlc alacritty lutris steam wine-stable discord gparted mc nano 
-sudo systemctl enable sddm.service
-sudo systemctl enable ufw
-sudo mkdir -p /etc/skel
-sudo cp -r skel/.config /etc/skel/.
+yay -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins sweet-kde-git dolphin-root packagekit-qt5 ufw firefox vlc alacritty lutris steam wine-stable discord gparted mc nano && \
+sudo systemctl enable sddm.service && \
+sudo systemctl enable ufw && \
+sudo mkdir -p /etc/skel && \
+sudo cp -r skel /etc
