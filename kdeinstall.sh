d46f30c655d3e5fbacebf30e287656ffd8ff9aea
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-04 17:26:01 +0100

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,5 +1,5 @@
 sudo pacman -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta && \
-yay -S --noconfirm dolphin-plugins dolphin-root packagekit-qt5 ufw clamtk firefox ferdi vlc alacritty libreoffice-fresh-es lutris steam wine discord gparted mc nano && \
+yay -S --noconfirm dolphin-plugins  packagekit-qt5 ufw clamtk firefox ferdi vlc alacritty libreoffice-fresh-es lutris steam wine discord gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable ufw && \
 sudo mkdir -p /etc/skel && \
