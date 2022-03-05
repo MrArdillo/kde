45812728d5dc6a216a29b69dbba6c550ce0c5926
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-05 14:42:03 +0100

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,7 +1,7 @@
-yay -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta sweet-theme-git dolphin-plugins packagekit-qt5 && \
+yay -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins packagekit-qt5 && \
 yay -S --noconfirm  ufw clamtk firefox ferdi-bin onedriver vlc alacritty libreoffice-fresh-es lutris steam wine discord gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable ufw && \
 sudo mkdir -p /etc/skel && \
 sudo cp -r skel /etc
-sudo cp -r Sweet /usr/share/themes/.
+
