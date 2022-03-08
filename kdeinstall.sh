595abc050e8f191919dfa5725a0322b9b1708e0a
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-08 20:29:26 +0100

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,7 +1,7 @@
 sudo pacman -Sy --noconfirm xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader && \
 yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
-yay -S --noconfirm  ufw clamtk firefox ferdi-git onedriver vlc alacritty libreoffice-fresh-es lutris steam wine gparted mc nano && \
+yay -S --noconfirm  ufw clamtk firefox discord onedriver vlc alacritty libreoffice-fresh-es lutris steam wine gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable ufw && \
 sudo systemctl enable fstrim.timer && \
