0d91bc1f742d8b3603bf9ba73050a652b4839c38
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-29 13:47:21 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,8 +1,7 @@
 sudo pacman -Sy --noconfirm xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader && \
 yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins optimus-manager optimus-manager-qt packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
-yay -S --noconfirm  ufw clamtk firefox-i18n-es-es discord onedriver flatpak vlc xow-git alacritty lutris steam wine heroic-games-launcher-bin gparted mc nano && \
-sudo flatpak install flathub org.onlyoffice.desktopeditors && \
+yay -S --noconfirm  ufw clamtk firefox-i18n-es-es discord onedriver libreoffice-fresh-es flatpak vlc xow-git alacritty lutris steam wine heroic-games-launcher-bin gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
 sudo systemctl enable ufw && \
