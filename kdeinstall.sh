57d83539772b4480d865c66fa04a598b68e870b8
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-29 16:58:59 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,4 +1,14 @@
-sudo pacman -Sy --noconfirm xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader && \
+echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >>/mnt/etc/pacman.conf
+if [[ $DG = s ]] || [[ $DG = S ]] || [[ $DG = si ]] || [[ $DG = Si ]]
+then
+	GPU='DESCONOCIDA'
+  (lspci | grep VGA) | grep -o 'NVIDIA' >/dev/null && GPU='nvidia'
+  case $GPU in
+  nvidia)
+			echo -e "\n>>Instalando drivers graficos de Nvidia\c"
+			echo "pacman --noconfirm -Sy xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader || exit 1" | ARCH && DONE || ERROR ;;
+      esac
+     fi && \
 yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins optimus-manager optimus-manager-qt packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
 yay -S --noconfirm  ufw clamtk firefox-i18n-es-es discord onedriver libreoffice-fresh-es flatpak vlc xow-git alacritty lutris steam wine heroic-games-launcher-bin gparted mc nano && \
