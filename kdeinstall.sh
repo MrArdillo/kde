2a972a721924f9dde25c992ae6e8d6d468d08144
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-29 20:46:01 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -11,13 +11,13 @@ then
 			echo "pacman --noconfirm -Sy xf86-video-vesa xf86-video-amdgpu lib32-mesa mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt  || exit 1" | ARCH && DONE || ERROR ;;
 		nvidia)
 			echo -e "\n>>Instalando drivers graficos de Nvidia\c"
-			echo "pacman --noconfirm -Sy xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt  egl-wayland  || exit 1" | ARCH && DONE || ERROR ;;
+			echo "pacman --noconfirm -Sy xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt   || exit 1" | ARCH && DONE || ERROR ;;
   		intel)
   			echo -e "\n>>Instalando drivers graficos Intel\c"
 			echo "pacman --noconfirm -Sy xf86-video-vesa xf86-video-intel lib32-mesa mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt   || exit 1" | ARCH && DONE || ERROR ;;
 esac
 fi && \		
-yay -S --noconfirm sddm plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta dolphin-plugins packagekit-qt5 print-manager cups system-config-printer && \
+yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
 yay -S --noconfirm  ufw clamtk firefox-i18n-es-es discord onedriver libreoffice-fresh-es flatpak vlc xow-git alacritty lutris steam wine heroic-games-launcher-bin gparted mc nano && \
 sudo systemctl enable sddm.service && \
