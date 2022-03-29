25ac827480deeb5bd2d0c4d6286da0e3321da2d9
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-03-29 17:26:35 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -2,13 +2,23 @@ echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >>/mnt/etc/pacman.con
 if [[ $DG = s ]] || [[ $DG = S ]] || [[ $DG = si ]] || [[ $DG = Si ]]
 then
 	GPU='DESCONOCIDA'
-  (lspci | grep VGA) | grep -o 'NVIDIA' >/dev/null && GPU='nvidia'
-  case $GPU in
-  nvidia)
+	(lspci | grep 3D controller) | grep -o 'Intel' >/dev/null && GPU='intel'
+	(lspci | grep 3D controller) | grep -o 'AMD' >/dev/null && GPU='amd'
+	(lspci | grep 3D controller) | grep -o 'NVIDIA' >/dev/null && GPU='nvidia'
+	case $GPU in
+		amd)
+			echo -e "\n>>Instalando drivers graficos de AMD\c"
+			echo "pacman --noconfirm -Sy xf86-video-vesa xf86-video-amdgpu lib32-mesa mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt  || exit 1" | ARCH && DONE || ERROR ;;
+		nvidia)
 			echo -e "\n>>Instalando drivers graficos de Nvidia\c"
-			echo "pacman --noconfirm -Sy xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt || exit 1" | ARCH && DONE || ERROR ;;
-      esac
-     fi && \
+			echo "pacman --noconfirm -Sy xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt   || exit 1" | ARCH && DONE || ERROR ;;
+  		intel)
+  			echo -e "\n>>Instalando drivers graficos Intel\c"
+			echo "pacman --noconfirm -Sy xf86-video-vesa xf86-video-intel lib32-mesa mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt   || exit 1" | ARCH && DONE || ERROR ;;
+esac
+fi && \		
+
+#pacman --noconfirm -Sy xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidia-dkms vulkan-icd-loader lib32-vulkan-icd-loader optimus-manager optimus-manager-qt  && \
 yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta dolphin-plugins packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
 yay -S --noconfirm  ufw clamtk firefox-i18n-es-es discord onedriver libreoffice-fresh-es flatpak vlc xow-git alacritty lutris steam wine heroic-games-launcher-bin gparted mc nano && \
