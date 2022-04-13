26b0e1ba8b9e4612c74a6101f461bcc11799e7a2
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-04-13 10:45:08 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -27,8 +27,7 @@ sudo systemctl enable fstrim.timer && \
 sudo systemctl enable samba.service && \
 sudo systemctl enable bluetooth.service && \
 sudo systemctl enable optimus-manager.service ; \
-sudo xow-get-firmware.sh && \
-y && \
+sudo bash xow-get-firmware.sh -y && \
 sudo systemctl enable xow && \
 sudo systemctl start xow && \
 echo "instalación completada"
