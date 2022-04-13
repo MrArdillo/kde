6d46db3307839caa213fd2052c37dae5e1d2f053
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-04-13 10:33:00 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -19,7 +19,7 @@ esac
 fi && \		
 yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta kaccounts-providers dolphin-plugins packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
-yay -S --noconfirm ufw clamtk firefox-i18n-es-es discord onedriver libreoffice-fresh-es okular flatpak vlc xow-git lutris steam wine heroic-games-launcher-bin gparted mc nano && \
+yay -S --noconfirm ufw clamtk firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xow-git lutris steam wine heroic-games-launcher-bin gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
 sudo systemctl enable ufw && \
@@ -28,6 +28,7 @@ sudo systemctl enable samba.service && \
 sudo systemctl enable bluetooth.service && \
 sudo systemctl enable optimus-manager.service ; \
 sudo xow-get-firmware.sh && \
+y && \
 sudo systemctl enable xow && \
 sudo systemctl start xow && \
 echo "instalación completada"
