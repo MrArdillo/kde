4433102348cbc7a8f8487541733b0fcc4ea93b36
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-04-25 16:49:24 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,7 +1,6 @@
 yay -S --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta kaccounts-providers dolphin-plugins packagekit-qt5 print-manager cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing && \
 yay -S --noconfirm ufw clamtk firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xow-git lutris steam wine heroic-games-launcher-bin gparted mc nano && \
-yay -S konsave && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
 sudo systemctl enable ufw && \
@@ -12,5 +11,4 @@ sudo systemctl enable optimus-manager.service ; \
 sudo xow-get-firmware.sh --skip-disclaimer && \
 sudo systemctl enable xow && \
 sudo systemctl start xow && \
-sudo konsave -i jorge.knsv && \
 echo "instalación completada"
