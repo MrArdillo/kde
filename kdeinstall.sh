a94ac7121f9cf4f6ca0c255492cee10b24a4f905
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-04-26 12:17:32 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -4,8 +4,8 @@ yay -S --noconfirm samba kdenetwork-filesharing && \
 yay -S --noconfirm ufw clamtk firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xow-git lutris steam wine heroic-games-launcher-bin gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
-su %USER konsave -i jorge.knsv && \
-su %USER konsave -a jorge && \
+su -c -p %USER konsave -i jorge.knsv && \
+su -c -p %USER konsave -a jorge.knsv && \
 sudo systemctl enable ufw && \
 sudo systemctl enable fstrim.timer && \
 sudo systemctl enable samba.service && \
