8cb775d29eb8738d1b2db63cb771957491e2be69
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-04-28 16:12:43 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,5 +1,5 @@
-yay -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta kaccounts-providers dolphin-plugins packagekit-qt5 spectacle print-manager cups system-config-printer candy-icons-git && \
-yay -S --noconfirm samba kdenetwork-filesharing && \
+yay -Sy --noconfirm sddm plasma-meta kde-system-meta kde-utilities-meta kaccounts-providers dolphin-plugins packagekit-qt5 spectacle print-manager cups system-config-printer && \
+yay -S --noconfirm samba kdenetwork-filesharing candy-icons-git && \
 yay -S --noconfirm ufw clamtk firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak flatseal vlc xow-git lutris steam wine piper heroic-games-launcher-bin gparted mc nano konsave && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
