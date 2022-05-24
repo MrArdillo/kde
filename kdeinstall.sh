b5cf6be192db5e15d45a0299a48fac3e4902b88c
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-05-25 01:13:24 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,6 +1,6 @@
 yay -Sy --noconfirm egl-wayland sddm plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kaccounts-providers dolphin-plugins packagekit-qt5 spectacle noto-fonts-emoji cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing candy-icons-git konsave && \
-yay -S --noconfirm ufw clamtk piper firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xone-dkms lutris steam wine heroic-games-launcher-bin gparted mc nano && \
+yay -S --noconfirm ufw clamtk piper firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xow-git lutris steam wine heroic-games-launcher-bin gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
 sudo systemctl enable ufw && \
