52faed21c93e0ce1dd437d3c14306a278b8e6d2b
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-05-25 02:06:01 +0200

    Update kdeinstall.sh

--- kdeinstall.sh
+++ kdeinstall.sh
@@ -1,14 +1,13 @@
 yay -Sy --noconfirm egl-wayland sddm plasma-meta plasma-wayland-session kde-system-meta kde-utilities-meta kaccounts-providers dolphin-plugins packagekit-qt5 spectacle noto-fonts-emoji cups system-config-printer && \
 yay -S --noconfirm samba kdenetwork-filesharing candy-icons-git konsave && \
-yay -S --noconfirm ufw clamtk piper firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xow-git lutris steam wine heroic-games-launcher-bin gparted mc nano && \
+yay -S --noconfirm ufw clamtk piper firefox-i18n-es-es discord onedriver ttf-ms-fonts libreoffice-fresh-es okular flatpak vlc xone-dkms lutris steam wine heroic-games-launcher-bin gparted mc nano && \
 sudo systemctl enable sddm.service && \
 sudo systemctl enable cups.service && \
 sudo systemctl enable ufw && \
 sudo systemctl enable fstrim.timer && \
 sudo systemctl enable samba.service && \
 sudo systemctl enable bluetooth.service && \
-sudo systemctl enable xow && \
-sudo xow-get-firmware.sh --skip-disclaimer && \
+#sudo xone-get-firmware.sh --skip-disclaimer && \
 sudo cp jorge.knsv /mnt/. && \
 sudo mkdir -p /etc/skel/Escritorio && \
 sudo cp Entorno_Jorge.sh /etc/skel/Escritorio/. && \
