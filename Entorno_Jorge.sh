864b957dc870641faaa81f79735b7eaef7ee4c86
MrArdillo <77178902+MrArdillo@users.noreply.github.com>
2022-06-19 18:40:49 +0200

    Update Entorno_Jorge.sh

--- Entorno_Jorge.sh
+++ Entorno_Jorge.sh
@@ -1,8 +1,2 @@
 konsave -i /mnt/jorge.knsv && \
-konsave -a jorge && \
-sudo xone-get-firmware.sh --skip-disclaimer && \
-echo -e "debe reiniciarse para completar el proceso. Desea hacerlo ahora (S/n)" && read ANS
-			if [[ $ANS = y ]] || [[ $ANS = yes ]] || [[ $ANS = YES ]] || [[ $ANS = Y ]]
-			then reboot
-			else exit
-			fi
+konsave -a jorge 
